635e3cd6385651d7b60d95f34f8203356097ff32
LDOrchard <43548000+LDOrchard@users.noreply.github.com>
2018-10-31 11:41:35 +0000

    Initial commit

---
+++ Assets/Scripts/SmoothFollow2D.cs
@@ -0,0 +1,143 @@
+﻿/*******************************************************
+ * 
+ * SmoothFollow2D.cs
+ *  - follows the transform of another GameObject
+ *  - uses a smoothing variable to animate movement over time
+ * 
+ * 
+ * 
+ * public variables:
+ *  - target
+ *    - the transform of the GameObject to follow
+ *    
+ *  - smoothing
+ *    - the speed of movement towards the target transform
+ *    
+ *    
+ * private variables: none
+ *    
+ *    
+ * Monobehaviour methods
+ *  - FixedUpdate
+ *    - runs contantly at a fixed time step
+ *    - only runs while this script is active
+ *    - see link: https://docs.unity3d.com/ScriptReference/MonoBehaviour.FixedUpdate.html
+ * 
+ * 
+ *******************************************************/
+using UnityEngine;
+ 
+public class SmoothFollow2D : MonoBehaviour
+{
+    /*********************
+     * 
+     * PUBLIC VARIABLES
+     * A "public" variable is declared outside of any methods.
+     * We can change these in the editor without changing any of the code.
+     * 
+     *********************/
+
+    /*
+     * target
+     * this is the Transform component belonging to the GameObject we want to follow
+     * set this variable in the editor by dragging the other GameObject onto this variable in the editor
+     * see link: https://docs.unity3d.com/ScriptReference/Transform.html
+     */
+    public Transform target;
+
+
+    /*
+     * smoothing
+     * smoothing is a float (a decimal number) to set how fast we move towards the target.
+     * smoothing has a default setting of "5.0f", this setting can be changed in the editor!
+     * NOTE: ALL float number values MUST end with an "f"
+     */
+    public float smoothing = 5.0f;
+
+
+    /*
+    * FixedUpdate
+    * A method (also known as an "Event Function") provided by Monobehaviour that will run at a fixed time step.
+    * FixedUpdate is most useful for the Physics system, such adding a force or velocity to a Rigidbody2D.
+    * see link: https://docs.unity3d.com/ScriptReference/MonoBehaviour.FixedUpdate.html
+    * 
+    * In this method we will get the position of this GameObject and the position of the target GameObject.
+    * Then we will use "Vector3.Lerp" to move towards the target GameObject over time
+    * "Lerp" is short for "Linear Interpolation"
+    * see link: https://docs.unity3d.com/ScriptReference/Vector3.Lerp.html
+    * 
+    * NOTE: we are using FixedUpdate because we will be "following" a Rigidbody2D, and it will look smoother if we 
+    *       use the same "update" method for both!
+    */
+    void FixedUpdate()
+    {
+        /*
+         * GET THIS GAMEOBJECT'S CURRENT POSITION
+         * we can get our current position from the Transform component using "transform.position"
+         * see link: https://docs.unity3d.com/ScriptReference/Transform-position.html
+         * "transform.position" is a Vector3, which has 3 properties: X, Y and Z.
+         * see link: https://docs.unity3d.com/ScriptReference/Vector3.html
+         * we create a Vector3 variable to store our transform's current position
+         */
+        Vector3 currentPosition = transform.position;
+
+
+        /*
+         * GET THE target GAMEOBJECT'S CURRENT POSITION
+         * the public variable, "target" is a Transform, so we can get its position by using
+         * "target.position", which will give us a Vector3
+         * we create a Vector3 variable to store our target's current position
+         */
+        Vector3 targetPosition = target.position;
+
+
+        /*
+         * SET OUR SMOOTHING TO A LOWER VALUE
+         * we need to reduce the size of the public variable "smoothing".
+         * If we don't, the speed will be instantaneous! (it's to do with the size of a "unit" in Unity)
+         * if we multiply smoothing by a very small number (or divide by a large number), it will give us a very small number to animate with
+         * we create a variable from multiplying smoothing by 0.001 (one thousandth!)
+         */ 
+        float moveSpeed = smoothing * 0.001f;
+
+
+        /*
+         * CREATE A Vector3 VARIABLE FOR OUR NEW POSITION
+         * We only want the X and Y position of the target, and keep our original Z position.
+         * This is becuase this script is meant for Camera movement.
+         * If the Camera's Z position is the same as the target's Z position, we wont be able to see it! the camera will "zoom" in too far!
+         */ 
+        Vector3 newPosition = new Vector3();
+
+
+        /*
+         * SET POSITION X TO THE TARGET X
+         * We want to move to the target X position
+         */ 
+        newPosition.x = targetPosition.x;
+
+
+        /*
+         * SET POSITION Y TO THE TARGET Y
+         * We want to move to the target Y position
+         */ 
+        newPosition.y = targetPosition.y;
+
+
+        /*
+         * SET THE POSITION Z TO OUR TRANSFORM POSITION Z
+         * We want to keep our original Z position to keep the camera's distance away from the target
+         */ 
+        newPosition.z = currentPosition.z;
+
+
+        /*
+         * USE "LERP" TO MOVE TO THE NEW POSITION
+         * "Lerp" is short for "Linear Interpolation" - or movement over time!
+         * we set our current position (transform.position) to move a little bit towards the target position every update
+         * see link: https://docs.unity3d.com/ScriptReference/Vector3.Lerp.html
+         * we give Vector3.Lerp our current position (currentPosition), the new position (newPosition) and a speed to move by (moveSpeed)
+         */
+        transform.position = Vector3.Lerp(currentPosition, newPosition, moveSpeed);
+    }
+}
---
+++ Assets/Scripts/TopDownCharacterController2D.cs
@@ -0,0 +1,175 @@
+﻿/*******************************************************
+ * 
+ * TopDownCharacterController2D.cs
+ *  - controls movement along the X and Y axes
+ *  - uses Rigidbody2D velocity to set movement
+ * 
+ * 
+ * public variables:
+ *  - speed
+ *    - the speed of movement
+ *    
+ *    
+ * private variables:
+ *  - body2D
+ *    - Rigidbody2D component
+ *    
+ *    
+ * Monobehaviour methods
+ *  - Start
+ *    - runs ONCE when this script is activated
+ *    - see link: https://docs.unity3d.com/ScriptReference/MonoBehaviour.Start.html
+ *    
+ *  - FixedUpdate
+ *    - runs contantly at a fixed time step
+ *    - only runs while this script is active
+ *    - see link: https://docs.unity3d.com/ScriptReference/MonoBehaviour.FixedUpdate.html
+ * 
+ * 
+ *******************************************************/
+using UnityEngine;
+
+public class TopDownCharacterController2D : MonoBehaviour
+{
+    /*
+     * speed
+     * A "public" variable is declared outside of any methods.
+     * We can change this in the editor without changing any of the code.
+     * speed has a default setting of "5.0f", this setting can be changed in the editor!
+     * 
+     * speed is a float (a decimal number) to set our movement speed.
+     * NOTE: ALL float number values MUST end with an "f"
+     */ 
+    public float speed = 5.0f;
+
+
+    /*
+     * body2D
+     * A "private" variable declared outside of any methods.
+     * This variable CANNOT be changed in the editor, it is "invisible" to the editor 
+     * and any other classes!
+     * 
+     * body2D is a Rigidbody2D component variable
+     * NOTE: we will store our Rigidbody2D component we added in the editor in this variable,
+     *       this is known as a "reference" as we are "referring" to the Rigidbody2D component.
+     * 
+     * see link: https://docs.unity3d.com/ScriptReference/Rigidbody2D.html
+     */
+    private Rigidbody2D body2D;
+
+
+    /*
+     * Start
+     * A method (also known as an "Event Function") provided by Monobehaviour that will run only once when a GameObject containing 
+     * this script activates in the Hierarchy.
+     * see link: https://docs.unity3d.com/ScriptReference/MonoBehaviour.Start.html
+     * 
+     * In this method we get the Rigidbody2D component using "GetComponent" and store it in
+     * out private variable, "body2D"
+     * see link: https://docs.unity3d.com/ScriptReference/Component.GetComponent.html
+     */
+    void Start()
+    {
+        /*
+         * GET THE RIGIDBODY2D COMPONENT AND STORE IN "body2D"
+         * We use GetComponent to get the Rigidbody2D component
+         * see link: https://docs.unity3d.com/ScriptReference/Component.GetComponent.html
+         * 
+         * we assign our "body2D" private variable to the Rigidbody2D component
+         * see link:  https://docs.unity3d.com/ScriptReference/Rigidbody2D.html
+         * 
+         * NOTE: GetComponent requires angle brackets to get the "type" of component.
+         *       We can get any type of component (including this script) this way!
+         */
+        body2D = GetComponent<Rigidbody2D>();
+    }
+
+
+    /*
+     * FixedUpdate
+     * A method (also known as an "Event Function") provided by Monobehaviour that will run at a fixed time step.
+     * FixedUpdate is most useful for the Physics system, such adding a force or velocity to a Rigidbody2D.
+     * see link: https://docs.unity3d.com/ScriptReference/MonoBehaviour.FixedUpdate.html
+     * 
+     * In this method we will get input from the X and Y (W,A,S,D keys) using Input.GetAxis()
+     * see link: https://docs.unity3d.com/ScriptReference/Input.GetAxis.html
+     * We will then create a Vector2 to store the X and Y input
+     * see link: https://docs.unity3d.com/ScriptReference/Vector2.html
+     * Then, we will multiply the Vector2 by our public variable "speed"
+     * Finally we will set the "velocity" property of our "body2D", this will move our character!
+     * see link: https://docs.unity3d.com/ScriptReference/Rigidbody2D-velocity.html
+     */
+    void FixedUpdate()
+    {
+        /*
+         * GETTING INPUT
+         * Unity provides some pre-made inputs for left and right (Horizontal) and up and down (Vertical) in the "Input Manager" panel in the editor.
+         * see link: https://docs.unity3d.com/Manual/class-InputManager.html
+         */
+
+
+        /*
+         * GET LEFT AND RIGHT INPUT (Horizontal)
+         * we create a local float variable and store our current "Horizontal" input using "Input.GetAxis"
+         * see link: https://docs.unity3d.com/ScriptReference/Input.GetAxis.html
+         * this will read our "A" and "D" keys and output a number from -1 to 1
+         * LEFT or "A" key is pressed = -1
+         * RIGHT or "D" key is pressed = 1
+         * Neither "A" or "D" key is pressed = 0
+         */
+        float inputX = Input.GetAxis("Horizontal");
+
+
+        /*
+         * GET UP AND DOWN INPUT (Vertical)
+         * we create a local float variable and store our current "Vertical" input using "Input.GetAxis"
+         * see link: https://docs.unity3d.com/ScriptReference/Input.GetAxis.html
+         * this will read our "W" and "S" keys and output a number from -1 to 1
+         * UP or "W" key is pressed = 1
+         * DOWN or "S" key is pressed = -1
+         * Neither "W" or "S" key is pressed = 0
+         */
+        float inputY = Input.GetAxis("Vertical");
+
+
+        /*
+         * CREATE A Vector2 TO STORE OUR NEW VELOCITY
+         * We want to set the velocity on our "body2D", but it needs a Vector2.
+         * A Vector2 has 2 properties: X and Y, we can set these to our "inputX" and "inputY" variables created above.
+         * see link: https://docs.unity3d.com/ScriptReference/Vector2.html
+         */
+        Vector2 newVelocity = new Vector2();
+
+
+        /*
+         * SET THE X PROPERTY OF THE "newVelocity" Vector2
+         * We set our "newVelocity" x property to "inputX" (our horizontal movement)
+         */
+        newVelocity.x = inputX;
+
+
+        /*
+         * SET THE Y PROPERTY OF THE "newVelocity" Vector2
+         * We set our "newVelocity" y property to "inputY" (our vertical movement)
+         */
+        newVelocity.y = inputY;
+
+
+        /*
+         * MULTIPLY THE "newVelocity" Vector2 BY "speed"
+         * We want to set the speed of our movement using the public variable "speed" so we can control the speed from the editor
+         * We can multiply a Vector2 by a float number to increase both its X and Y properties
+         * see link: https://docs.unity3d.com/ScriptReference/Vector2-operator_multiply.html
+         */
+        newVelocity = newVelocity * speed;
+
+
+        /*
+         * SET THE "body2D" velocity TO "newVelocity"
+         * we simply set the velocity here
+         * "body2D" is a Rigidbody2D component, which has a "velocity" property of type Vector2
+         * see link: https://docs.unity3d.com/ScriptReference/Rigidbody2D-velocity.html
+         */
+        body2D.velocity = newVelocity;
+    }
+}
