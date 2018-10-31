8a65b04edb5b63ce38b7e1e69c4d10208e81f65d
LDOrchard <43548000+LDOrchard@users.noreply.github.com>
2018-10-31 11:58:32 +0000

    End of lesson Week 6
    Working with zombie spawner (not finished)

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject PrefabMob;
+    
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        Instantiate(PrefabMob);
+    }
+}
